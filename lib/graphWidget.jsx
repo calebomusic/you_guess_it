c48207964831f7c7141be4f52d0bdd779d23244b
calebomusic <calebomusic@gmail.com>
2017-03-05 15:08:52 -0800

    Complete skeleton

--- lib/graphWidget.jsx
+++ lib/graphWidget.jsx
@@ -1,19 +1,9 @@
 const generateGraph = require("./generateGraph.js");
 import React from 'react';
 
-// Include props for the question, beforeGuess button, and answertext
-
-// Make div for question, beforeGuess button, answerText
-  // Make sure answerText can be rendered as HTML
-  // Maybe just take in those as a react Component
-
-// props = options, graphId, question, button, answerText
-
-// TODO: move data xAxis and yAxis titles to options
-
 class GraphWidget extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   componentDidMount() {
@@ -22,23 +12,21 @@ class GraphWidget extends React.Component {
 
   render() {
     return(
-      <div></div>
+      <div id={'graphWidget-' + props.graphId}>
+        <div className={'before-' + props.graphId}>
+
+        </div>
+        <div id={'beforeGuess-' + props.graphId}
+             className={'beforeGuessComplete-' + props.graphId}>
+
+        </div>
+        <div className="hidden"
+             id={'answerText-' + props.graphId}>
+
+        </div>
+      </div>
     )
   }
 }
 
 export default GraphWidget;
-
-// <div id={'graphWidget-' + props.graphId}>
-//   <div className={'before-' + props.graphId}>
-//
-//   </div>
-//   <div id={'beforeGuess-' + props.graphId}
-//        className={'beforeGuessComplete-' + props.graphId}>
-//
-//   </div>
-//   <div className="hidden"
-//        id={'answerText-' + props.graphId}>
-//
-//   </div>
-// </div>
